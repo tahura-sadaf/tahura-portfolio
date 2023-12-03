017f1a63dc3e081408192ed068d732e7c8ca958f
tahura-sadaf <152806795+tahura-sadaf@users.noreply.github.com>
2023-12-03 23:40:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,142 @@
+/* ----- NAVIGATION BAR FUNCTION ----- */
+function myMenuFunction(){
+    var menuBtn = document.getElementById("myNavMenu");
+    if(menuBtn.className === "nav-menu"){
+      menuBtn.className += " responsive";
+    } else {
+      menuBtn.className = "nav-menu";
+    }
+  }
+
+
+  function openModal() {
+    document.getElementById('hireModal').style.display = 'block';
+  }
+  function closeModal() {
+    document.getElementById('hireModal').style.display = 'none';
+  }
+
+  
+let slideIndex = 0;
+let slideInterval;
+
+// Function to change slides
+function changeSlide(n) {
+  showSlides(slideIndex += n);
+  clearInterval(slideInterval); // Reset the timer
+  slideInterval = setInterval(() => {
+    showSlides(slideIndex += 1);
+  }, 7000); // Change slide every 5 seconds
+}
+
+// Function to show slides
+function showSlides(n) {
+  const slides = document.getElementsByClassName("mySlides");
+
+  if (n >= slides.length) {
+    slideIndex = 0;
+  }
+  if (n < 0) {
+    slideIndex = slides.length - 1;
+  }
+  
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex].style.display = "block";
+}
+
+// Initialize slideshow when the page loads
+window.onload = function() {
+  showSlides(slideIndex);
+  slideInterval = setInterval(() => {
+    showSlides(slideIndex += 1);
+  }, 5000); // Change slide every 5 seconds
+};
+
+
+/* ----- ADD SHADOW ON NAVIGATION BAR WHILE SCROLLING ----- */
+  window.onscroll = function() {headerShadow()};
+  function headerShadow() {
+    const navHeader =document.getElementById("header");
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop >  50) {
+      navHeader.style.boxShadow = "0 1px 6px rgba(0, 0, 0, 0.1)";
+      navHeader.style.height = "70px";
+      navHeader.style.lineHeight = "70px";
+    } else {
+      navHeader.style.boxShadow = "none";
+      navHeader.style.height = "90px";
+      navHeader.style.lineHeight = "90px";
+    }
+  }
+
+/*----- TYPING EFFECT ----- */
+var typingEffect = new Typed(".typedText", {
+    strings: ["Tahura Sadaf"],
+    typeSpeed: 100,
+    startDelay: 500, // Optional delay before typing starts
+    loop: false // Remove the loop to type the name only once
+});
+
+/* ----- ## -- SCROLL REVEAL ANIMATION -- ## ----- */
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true     
+})
+/* -- HOME -- */
+sr.reveal('.featured-text-card',{})
+sr.reveal('.featured-name',{delay: 100})
+sr.reveal('.featured-text-info',{delay: 200})
+sr.reveal('.featured-text-btn',{delay: 200})
+sr.reveal('.social_icons',{delay: 200})
+sr.reveal('.featured-image',{delay: 300})
+
+/* -- PROJECT BOX -- */
+sr.reveal('.project-box',{interval: 200})
+sr.reveal('.slideshow-container',{interval: 200})
+
+/* -- HEADINGS -- */
+sr.reveal('.top-header',{})
+
+/* ----- ## -- SCROLL REVEAL LEFT_RIGHT ANIMATION -- ## ----- */
+  /* -- ABOUT INFO & CONTACT INFO -- */
+  const srLeft = ScrollReveal({
+    origin: 'left',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+  })
+  
+  srLeft.reveal('.about-info',{delay: 100})
+  srLeft.reveal('.contact-info',{delay: 100})
+  /* -- ABOUT SKILLS & FORM BOX -- */
+  const srRight = ScrollReveal({
+    origin: 'right',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+  })
+  
+  srRight.reveal('.skills-box',{delay: 100})
+  srRight.reveal('.form-control',{delay: 100})
+  
+/* ----- CHANGE ACTIVE LINK ----- */
+  
+  const sections = document.querySelectorAll('section[id]')
+  function scrollActive() {
+    const scrollY = window.scrollY;
+    sections.forEach(current =>{
+      const sectionHeight = current.offsetHeight,
+          sectionTop = current.offsetTop - 50,
+        sectionId = current.getAttribute('id')
+      if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) { 
+          document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
+      }  else {
+        document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
+      }
+    })
+  }
+  window.addEventListener('scroll', scrollActive)
